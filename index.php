0fef78e9593306b314e4927a2d9736380bc6b463
Trismegiste <flo@trismegiste.net>
2014-10-28 13:58:55 +0100

    removing internal edge effet

--- index.php
+++ index.php
@@ -9,8 +9,6 @@ class SerialIcer
     const UUID_KEY = '@uuid';
     const REF_KEY = '@ref';
 
-    protected $reference = [];
-
     /**
      * Get the closure to export an object with a class scope
      * 
@@ -20,53 +18,52 @@ class SerialIcer
     {
         $that = $this; // smells like javascript...
 
-        return function ($scope, array& $export) use ($that) {
+        return function ($scope, array& $export, array& $ref) use ($that) {
                     $refl = new \ReflectionClass($scope);
 
                     foreach ($refl->getProperties() as $prop) {
                         if ($prop->class === $scope) {
                             $key = $prop->name;
-                            $export[$scope . '::' . $key] = $that->export($this->$key);
+                            $export[$scope . '::' . $key] = $that->export($this->$key, $ref);
                         }
                     }
                 };
     }
 
-    public function exportObj($obj)
+    public function exportObj($obj, array& $ref)
     {
         $addr = spl_object_hash($obj);
-        if (array_key_exists($addr, $this->reference)) {
+        if (array_key_exists($addr, $ref)) {
             return [self::REF_KEY => $addr];
         }
         $scope = get_class($obj);
         $export = [self::CLASS_KEY => $scope, self::UUID_KEY => $addr];
-        $this->reference[$addr] = true;
+        $ref[$addr] = true;
 
         $flatten = $this->getExportClosure();
         do {
             $dump = \Closure::bind($flatten, $obj, $scope);
-            $dump($scope, $export);
+            $dump($scope, $export, $ref);
         } while ($scope = get_parent_class($scope));
 
         return $export;
     }
 
-    public function export($mixed)
+    public function export($mixed, array& $ref = [])
     {
         if (is_scalar($mixed)) {
             return $mixed;
         } elseif (is_array($mixed)) {
             $arr = [];
             foreach ($mixed as $key => $value) {
-                $arr[$key] = $this->export($value);
+                $arr[$key] = $this->export($value, $ref);
             }
 
             return $arr;
         } elseif (is_object($mixed)) {
-            return $this->exportObj($mixed);
+            return $this->exportObj($mixed, $ref);
         }
 
-        //throw new \RuntimeException("wtf");
         return null;
     }
 
@@ -78,11 +75,11 @@ class SerialIcer
      * 
      * @throws \RuntimeException
      */
-    public function create(array $import)
+    public function create(array $import, array& $ref = [])
     {
         if (array_key_exists(self::REF_KEY, $import)) {
-            if (array_key_exists($import[self::REF_KEY], $this->reference)) {
-                return $this->reference[$import[self::REF_KEY]];
+            if (array_key_exists($import[self::REF_KEY], $ref)) {
+                return $ref[$import[self::REF_KEY]];
             } else {
                 throw new \RuntimeException($import[self::REF_KEY] . ' ref is unknown');
             }
@@ -91,29 +88,27 @@ class SerialIcer
         if (array_key_exists(self::CLASS_KEY, $import)) {
             $scope = $import[self::CLASS_KEY];
             $obj = $this->instantiate($scope);
-            $this->reference[$import[self::UUID_KEY]] = $obj;
+            $ref[$import[self::UUID_KEY]] = $obj;
             unset($import[self::CLASS_KEY]);
             unset($import[self::UUID_KEY]);
             $hydrate = $this->getImportClosure();
             do {
                 $dump = \Closure::bind($hydrate, $obj, $scope);
-                $dump($scope, $import);
+                $dump($scope, $import, $ref);
             } while ($scope = get_parent_class($scope));
 
             return $obj;
-        } else {
-            $arr = [];
-            foreach ($import as $key => $value) {
-                if (is_array($value)) {
-                    $value = $this->create($value);
-                }
-                $arr[$key] = $value;
-            }
-
-            return $arr;
         }
 
-        throw new \RuntimeException('fail');
+        $arr = [];
+        foreach ($import as $key => $value) {
+            if (is_array($value)) {
+                $value = $this->create($value, $ref);
+            }
+            $arr[$key] = $value;
+        }
+
+        return $arr;
     }
 
     protected function instantiate($cls)
@@ -127,22 +122,17 @@ class SerialIcer
     {
         $that = $this;
 
-        return function($scope, array $data) use ($that) {
+        return function($scope, array $data, array& $ref) use ($that) {
                     foreach ($data as $key => $value) {
                         list($fqcn, $prop) = explode('::', $key);
                         if ($fqcn === $scope) {
                             if (is_array($value)) {
-                                $value = $that->create($value);
+                                $value = $that->create($value, $ref);
                             }
                             $this->$prop = $value;
                         }
                     }
                 };
-    }
-
-    public function reset()
-    {
-        $this->reference = [];
     }
 
 }
@@ -153,7 +143,6 @@ $obj = new Company(new Employee('toto', 13));
 
 $export = $service->export($obj);
 print_r($export);
-$service->reset();
 $newObj = $service->create($export);
 print_r($obj);
 print_r($newObj);
